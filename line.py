3eeaa5f1073296f5aa00ab81b858009253df1cf0
Muhammad Furqan <drmfurqan@gmail.com>
2018-10-28 12:01:14 +0500

    player related actions added

--- line.py
+++ line.py
@@ -1,5 +1,5 @@
 from box import Box
-from config import BOXES
+from config import BOXES, players
 
 
 class Line:
@@ -15,7 +15,8 @@ class Line:
                         line=line, canvas=canvas: self.selectLine(line, canvas))
 
     def selectLine(self, line_object, canvas):
-        canvas.itemconfig(line_object, fill='#ff931e')
+        global players
+        canvas.itemconfig(line_object, fill=players[0].color)
         self.updateBoxes(BOXES)
         # updating box and score is yet to implement
 
@@ -24,25 +25,60 @@ class Line:
 
 
 class VerticalLine(Line):
+
     def __init__(self, points, canvas):
         Line.__init__(self, points, canvas)
 
     def updateBoxes(self, boxes):
+        global players
         if self.x1 > 0 and self.x1 < len(boxes[0]):
             box1 = boxes[self.y1][self.x1]
             box1.walls[3] = 1
             box2 = boxes[self.y1][self.x1 - 1]
             box2.walls[1] = 1
-            box1.displayScore(self.canvas, 100, 50)
-            box2.displayScore(self.canvas, 100, 50)
+
+            if box1.getScore() == 4 and box2.getScore() == 4:
+                box1.owned = players[0].name
+                players[0].owned += 1
+                box1.displayOwner(self.canvas, 100, 50)
+                # box1.displayScore(self.canvas, 100, 50)
+                box2.owned = players[0].name
+                players[0].owned += 1
+                box2.displayOwner(self.canvas, 100, 50)
+            elif box1.getScore() == 4:
+                box1.owned = players[0].name
+                players[0].owned += 1
+                box1.displayOwner(self.canvas, 100, 50)
+                # box1.displayScore(self.canvas, 100, 50)
+            elif box2.getScore() == 4:
+                box2.owned = players[0].name
+                players[0].owned += 1
+                box2.displayOwner(self.canvas, 100, 50)
+            else:
+                p1, p2 = players
+                players = [p2, p1]
         if self.x1 == 0:
             box = boxes[self.y1][self.x1]
             box.walls[3] = 1
-            box.displayScore(self.canvas, 100, 50)
+            if box.getScore() == 4:
+                box.owned = players[0].name
+                players[0].owned += 1
+                box.displayOwner(self.canvas, 100, 50)
+                # box.displayScore(self.canvas, 100, 50)
+            else:
+                p1, p2 = players
+                players = [p2, p1]
         if self.x1 == len(boxes[0]):
             box = boxes[self.y1][self.x1 - 1]
             box.walls[1] = 1
-            box.displayScore(self.canvas, 100, 50)
+            if box.getScore() == 4:
+                box.owned = players[0].name
+                players[0].owned += 1
+                box.displayOwner(self.canvas, 100, 50)
+                # box.displayScore(self.canvas, 100, 50)
+            else:
+                p1, p2 = players
+                players = [p2, p1]
 
 
 class HorizontalLine(Line):
@@ -50,18 +86,51 @@ class HorizontalLine(Line):
         Line.__init__(self, points, canvas)
 
     def updateBoxes(self, boxes):
+        global players
         if self.y1 > 0 and self.y1 < len(boxes[0]):
             box1 = boxes[self.y1][self.x1]
             box1.walls[0] = 1
             box2 = boxes[self.y1 - 1][self.x1]
             box2.walls[2] = 1
-            box1.displayScore(self.canvas, 100, 50)
-            box2.displayScore(self.canvas, 100, 50)
+            if box1.getScore() == 4 and box2.getScore() == 4:
+                box1.owned = players[0].name
+                players[0].owned += 1
+                box1.displayOwner(self.canvas, 100, 50)
+                box2.owned = players[0].name
+                players[0].owned += 1
+                box2.displayOwner(self.canvas, 100, 50)
+            elif box1.getScore() == 4:
+                box1.owned = players[0].name
+                players[0].owned += 1
+                box1.displayOwner(self.canvas, 100, 50)
+                # box1.displayScore(self.canvas, 100, 50)
+            elif box2.getScore() == 4:
+                box2.owned = players[0].name
+                players[0].owned += 1
+                box2.displayOwner(self.canvas, 100, 50)
+                # box2.displayScore(self.canvas, 100, 50)
+            else:
+                p1, p2 = players
+                players = [p2, p1]
         if self.y1 == 0:
             box = boxes[self.y1][self.x1]
             box.walls[0] = 1
-            box.displayScore(self.canvas, 100, 50)
+            if box.getScore() == 4:
+                box.owned = players[0].name
+                players[0].owned += 1
+                box.displayOwner(self.canvas, 100, 50)
+                # box.displayScore(self.canvas, 100, 50)
+            else:
+                p1, p2 = players
+                players = [p2, p1]
         if self.y1 == len(boxes[0]):
             box = boxes[self.y1 - 1][self.x1]
             box.walls[2] = 1
-            box.displayScore(self.canvas, 100, 50)
+            if box.getScore() == 4:
+                box.owned = players[0].name
+                players[0].owned += 1
+                box.displayOwner(self.canvas, 100, 50)
+                # box.displayScore(self.canvas, 100, 50)
+            else:
+                p1, p2 = players
+                players = [p2, p1]
