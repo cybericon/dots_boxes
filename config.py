1c47131ec4f98634b697bbfed78b9dcb7f5cda35
Muhammad Furqan <drmfurqan@gmail.com>
2018-10-28 12:00:31 +0500

    players and owned boxes added

--- config.py
+++ config.py
@@ -1,4 +1,15 @@
 from box import Box
+from player import Player
+
 
 SIZE = 8
 BOXES = [[Box(x, y) for x in range(SIZE)] for y in range(SIZE)]
+playerOne = Player("Touseef", "#ff931e")
+playerTwo = Player("Furqan",  "#2c3e50")
+players = [playerOne, playerTwo]
+
+boxes_owned = len(
+    [box.owned for row in BOXES for box in row if box.owned is not None])
+
+total_boxes = SIZE * SIZE
+available_boxes = total_boxes - boxes_owned
