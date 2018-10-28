6b220e093afd05138e9e02bdf1420a2f53d41b87
Muhammad Furqan <drmfurqan@gmail.com>
2018-10-28 15:42:29 +0500

    updated config file

--- config.py
+++ config.py
@@ -2,14 +2,21 @@ from box import Box
 from player import Player
 
 
-SIZE = 8
+SIZE = 4
+
 BOXES = [[Box(x, y) for x in range(SIZE)] for y in range(SIZE)]
 playerOne = Player("Touseef", "#ff931e")
 playerTwo = Player("Furqan",  "#2c3e50")
 players = [playerOne, playerTwo]
 
-boxes_owned = len(
-    [box.owned for row in BOXES for box in row if box.owned is not None])
-
 total_boxes = SIZE * SIZE
-available_boxes = total_boxes - boxes_owned
+
+
+def boxes_owned():
+    global BOXES
+    return len([box.owned for row in BOXES for box in row if box.owned is not None])
+
+
+def available_boxes():
+    global total_boxes
+    return total_boxes - boxes_owned()
