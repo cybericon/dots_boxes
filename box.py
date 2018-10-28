7fdc09c57326a3ffc917d446f1c4645f86b5088a
Muhammad Furqan <drmfurqan@gmail.com>
2018-10-28 12:00:09 +0500

    added getScore and displayOwne methods

--- box.py
+++ box.py
@@ -8,12 +8,15 @@ class Box:
         self.y = y
         self.walls = [0, 0, 0, 0]
         self.closed = 0
-        self.owned = 0
+        self.owned = None
 
         Box.boxes += 1
 
     def __str__(self):
         return f"Box coordinates ({self.x, self.y})"
+
+    def getScore(self):
+        return sum(self.walls)
 
     def displayScore(self, canvas, offset, gap):
         # return sum(self.walls)
@@ -23,3 +26,10 @@ class Box:
         canvas.create_oval(x + 5, y + 5, x + 45, y +
                            45, fill='#ecf0f1', outline="")
         canvas.create_text(x + 25, y + 25, text=str(score))
+
+    def displayOwner(self, canvas, offset, gap):
+        (x, y) = [point*gap+offset for point in [
+            self.x, self.y]]
+        canvas.create_oval(x + 5, y + 5, x + 45, y +
+                           45, fill='#ecf0f1', outline="")
+        canvas.create_text(x + 25, y + 25, text=self.owned[:3])
