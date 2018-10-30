627185a1250ff31658b25738f35128ad2ceaa722
Muhammad Furqan <drmfurqan@gmail.com>
2018-10-30 06:45:54 +0500

    refactored box class

--- box.py
+++ box.py
@@ -3,9 +3,7 @@ class Box:
     owned = 0
 
     def __init__(self, x, y):
-        # self.coordinates = (x, y)
-        self.x = x
-        self.y = y
+        self.x, self.y = x, y
         self.walls = [0, 0, 0, 0]
         self.closed = 0
         self.owned = None
@@ -18,18 +16,12 @@ class Box:
     def getScore(self):
         return sum(self.walls)
 
-    def displayScore(self, canvas, offset, gap):
-        # return sum(self.walls)
-        score = sum(self.walls)
-        (x, y) = [point*gap+offset for point in [
-            self.x, self.y]]
-        canvas.create_oval(x + 5, y + 5, x + 45, y +
-                           45, fill='#ecf0f1', outline="")
-        canvas.create_text(x + 25, y + 25, text=str(score))
-
     def displayOwner(self, canvas, offset, gap):
         (x, y) = [point*gap+offset for point in [
             self.x, self.y]]
-        canvas.create_oval(x + 5, y + 5, x + 45, y +
-                           45, fill='#ecf0f1', outline="")
-        canvas.create_text(x + 25, y + 25, text=self.owned[:3])
+        if self.owned != None:
+            initials = self.owned[:3]
+        else:
+            initials = ''
+
+        canvas.create_text(x + 25, y + 25, text=initials)
