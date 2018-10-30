d942d608e273464f4ff29217dbc5341ff5fcbca0
Muhammad Furqan <drmfurqan@gmail.com>
2018-10-30 06:47:56 +0500

    cleaned player class

--- player.py
+++ player.py
@@ -5,18 +5,3 @@ class Player:
         self.name = name
         self.owned = 0
         self.color = color
-
-    def move(self):
-        pass
-
-    # def move(self, x, y):
-    #     # check if x and y are valid points
-    #     # check if the line between x and y is valid and not drawn already
-    #     # mark the line drawn
-    #     # update related boxes
-
-    #     # check if box is complete
-    #         # add box to collection
-    #         # move again
-
-    #     pass
