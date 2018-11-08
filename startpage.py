d91e3bbeb1aa0ae08379934b9608d433c5ac301a
Muhammad Furqan <drmfurqan@gmail.com>
2018-11-08 17:28:25 +0500

    resized stage and scoreboard

--- startpage.py
+++ startpage.py
@@ -1,7 +1,7 @@
 import tkinter as tk
 from tkinter.font import Font
 
-from core import scoreboard, stage
+from core import scoreboard, stage, copyrights
 
 
 class StartPage(tk.Frame):
@@ -10,11 +10,15 @@ class StartPage(tk.Frame):
         self._light_bg = "#ecf0f1"
         self._dark_bg = "#2c3e50"
 
-        stage.config(self, width=550, height=600,
+        stage.config(self, width=550, height=500,
                      background=self._light_bg)
 
-        scoreboard.config(self, width=230, height=600,
+        scoreboard.config(self, width=230, height=500,
                           background=self._dark_bg)
+
+        copyrights.config(self, width=780, height=120,
+                          background=self._light_bg)
 
         stage.show()
         scoreboard.show()
+        copyrights.show()
